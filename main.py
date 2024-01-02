47e5dd2ce74e7fab87909da18887fafe8323c8ca
Pat Hearps <pat.hearps@gmail.com>
2024-01-02 15:00:22 +1100

    FEAT: add enemy class

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 from pygame.locals import (
     K_UP,
@@ -37,6 +39,28 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
         if self.rect.bottom >= SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
+
+# Define the enemy object by extending pygame.sprite.Sprite
+# The surface you draw on the screen is now an attribute of 'enemy'
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),  # spawns somewhere off right edge of screen, random distance
+                random.randint(0, SCREEN_HEIGHT),  # spawns somewhere between bottom and top of screen
+            )
+        )
+        self.speed = random.randint(5, 20)
+
+    # Move the sprite based on speed
+    # Remove the sprite when it passes the left edge of the screen
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
 
 pygame.init()
 
