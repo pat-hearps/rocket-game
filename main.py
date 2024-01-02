710207a1b13e18ec9481a3d745732d2e5ae13a06
Pat Hearps <pat.hearps@gmail.com>
2024-01-02 14:56:50 +1100

    FEAT:  control speed

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pygame.locals import (
     QUIT,
 )
 
-
+MOVE_RATE = 2
 # Define a Player object by extending pygame.sprite.Sprite
 # The surface drawn on the screen is now an attribute of 'player'
 class Player(pygame.sprite.Sprite):
@@ -21,13 +21,13 @@ class Player(pygame.sprite.Sprite):
     # Move the sprite based on user keypresses
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
+            self.rect.move_ip(0, -MOVE_RATE)
         if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
+            self.rect.move_ip(0, MOVE_RATE)
         if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
+            self.rect.move_ip(-MOVE_RATE, 0)
         if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+            self.rect.move_ip(MOVE_RATE, 0)
         # Keep player on the screen
         if self.rect.left < 0:
             self.rect.left = 0
